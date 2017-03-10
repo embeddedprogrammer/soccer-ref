74902b7ed19a83395b6aec0c3f7fdbc3daf4eb33
Unknown <snowflakeobsidian3.14@gmail.com>
2017-03-10 15:41:37 -0700

    Get to compile. Add visulization code.

--- src/vision.cpp
+++ src/vision.cpp
@@ -90,25 +90,21 @@ void getBallPose(Mat& imgHsv, Scalar color[], geometry_msgs::Pose2D& ballPose)
 	if (hierarchy.size() < 1)
 		return;
 
+	// Sort
+	vector<Moments> mm;
 	for(int i = 0; i < hierarchy.size(); i++)
 		mm.push_back(moments((Mat)contours[i]));
-
 	std::sort(mm.begin(), mm.end(), compareMomentAreas);
 
-	Moments mm = moments((Mat)contours[0]);
-	Point2d ballCenter = imageToWorldCoordinates(getCenterOfMass(mm));
-
+	// Use largest to calculate ball center
+	Point2d ballCenter = imageToWorldCoordinates(getCenterOfMass(mm[0]));
 	ballPose.x = ballCenter.x;
 	ballPose.y = ballCenter.y;
 	ballPose.theta = 0;
 }
 
-void processImage(Mat img)
+void processImage(Mat img, Mat hsv)
 {
-	// Convert to HSV
-	Mat imgHsv;
-	cvtColor(img, hsv, COLOR_BGR2HSV);
-
 	// Threshold, etc
 	getBallPose(hsv,  yellow, poseBall);
 
@@ -116,31 +112,158 @@ void processImage(Mat img)
 	ball_pub.publish(poseBall);
 }
 
+void channelDist(Mat& hsv, Mat& dist, int hueVal, int channel)
+{
+	Mat hue;
+	extractChannel(hsv, hue, channel);
+	if (channel == 0)
+	{
+		Mat dist1, dist2;
+		absdiff(hue, hueVal, dist1);
+		absdiff(hue, hueVal + ((hueVal < 90) ? 180 : -180), dist2);
+		min(dist1, dist2, dist);
+	}
+	else
+		absdiff(hue, hueVal, dist);
+}
+
+void hueMax(Mat& hsv, Mat& img)
+{
+	vector<Mat> channels;
+	split(hsv, channels);
+	channels[1].setTo(255);
+	channels[2].setTo(255);
+	Mat hsv2;
+	merge(channels, hsv2);
+	cvtColor(hsv2, img, CV_HSV2BGR);
+}
+
+void detectLines(Mat& img)
+{
+	// Init
+	static Ptr<LineSegmentDetector> ls = createLineSegmentDetector(LSD_REFINE_STD); //can use LSD_REFINE_STD or LSD_REFINE_NONE
+
+	// Convert to grayscale
+    Mat gray;
+    cvtColor(img, gray, COLOR_BGR2GRAY);
+
+    // Detect the lines
+    vector<Vec4f> lines_std;
+    ls->detect(gray, lines_std);
+
+    // Show found lines
+    ls->drawSegments(img, lines_std);
+}
+
+void showHistogram(Mat& img)
+{
+	int bins = 256;             // number of bins
+	int nc = img.channels();    // number of channels
+
+	vector<Mat> hist(nc);       // histogram arrays
+
+	// Initalize histogram arrays
+	for (int i = 0; i < hist.size(); i++)
+		hist[i] = Mat::zeros(1, bins, CV_32SC1);
+
+	// Calculate the histogram of the image
+	for (int i = 0; i < img.rows; i++)
+	{
+		for (int j = 0; j < img.cols; j++)
+		{
+			for (int k = 0; k < nc; k++)
+			{
+				uchar val = nc == 1 ? img.at<uchar>(i,j) : img.at<Vec3b>(i,j)[k];
+				hist[k].at<int>(val) += 1;
+			}
+		}
+	}
+
+	// For each histogram arrays, obtain the maximum (peak) value
+	// Needed to normalize the display later
+	int hmax[3] = {0,0,0};
+	for (int i = 0; i < nc; i++)
+	{
+		for (int j = 0; j < bins-1; j++)
+			hmax[i] = hist[i].at<int>(j) > hmax[i] ? hist[i].at<int>(j) : hmax[i];
+	}
+
+	//const char* wname[3] = { "blue", "green", "red" };
+	//Scalar colors[3] = { Scalar(255,0,0), Scalar(0,255,0), Scalar(0,0,255) };
+	const char* wname[3] = { "hue", "sat", "val" };
+	Scalar colors[3] = { Scalar(255,255,255), Scalar(255,255,255), Scalar(255,255,255) };
+
+	vector<Mat> canvas(nc);
+
+	// Display each histogram in a canvas
+	for (int i = 0; i < nc; i++)
+	{
+		canvas[i] = Mat::ones(125, bins, CV_8UC3);
+
+		for (int j = 0, rows = canvas[i].rows; j < bins-1; j++)
+		{
+			line(
+				canvas[i], 
+				Point(j, rows), 
+				Point(j, rows - (hist[i].at<int>(j) * rows/hmax[i])), 
+				nc == 1 ? Scalar(200,200,200) : colors[i], 
+				1, 8, 0
+			);
+		}
+		imshow(nc == 1 ? "value" : wname[i], canvas[i]);
+	}
+}
+
+char lastKeyPressed;
+int hueVal = 0;
+Mat hsv, img, bgr;
+
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
-	try
-	{
-		Mat frame = cv_bridge::toCvShare(msg, "bgr8")->image;
-		processImage(frame);
-		imshow(GUI_NAME, frame);
-		waitKey(30);
-	}
-	catch (cv_bridge::Exception& e)
-	{
-		ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-	}
+	bgr = cv_bridge::toCvShare(msg, "bgr8")->image;
+	img = bgr;
+	cvtColor(img, hsv, COLOR_BGR2HSV);
+	processImage(img, hsv);
+
+	if(lastKeyPressed == 'h')
+		extractChannel(hsv, img, 0);
+	if(lastKeyPressed == 'i')
+		hueMax(hsv, img);
+	if(lastKeyPressed == 's')
+		extractChannel(hsv, img, 1);
+	if(lastKeyPressed == 'v')
+		extractChannel(hsv, img, 2);
+	if(lastKeyPressed == 'd')
+		channelDist(hsv, img, hueVal, 0);
+	if(lastKeyPressed == 'a')
+		img = hsv;
+	if(lastKeyPressed == 'l')
+		detectLines(img);
+	if(lastKeyPressed == 't')
+		showHistogram(hsv);
+
+	imshow(GUI_NAME, img);
+
+	// Wait for key press
+	char key = waitKey(30);
+	if(key != -1)
+		lastKeyPressed = key;
+	if(key == 'q')
+		ros::shutdown();
 }
 
 void mouseCallback(int event, int x, int y, int flags, void* userdata)
 {
+	Vec3b bgrVal = bgr.at<Vec3b>(y, x);
+	Vec3b hsvVal = hsv.at<Vec3b>(y, x);
+	char buffer[100];
+	sprintf(buffer, "B:%d G:%d R:%d - H:%d S:%d V:%d", bgrVal[0], bgrVal[1], bgrVal[2], hsvVal[0], hsvVal[1], hsvVal[2]);
+	displayStatusBar(GUI_NAME, buffer, 10000);
+
 	if (event == EVENT_LBUTTONDOWN)
-	{
-		mouse_left_down = true;
-		Point2d point_meters = imageToWorldCoordinates(Point2d(x, y));
-		char buffer[50];
-		sprintf(buffer, "Location: (%.3f m, %.3f m)", point_meters.x, point_meters.y);
-		displayStatusBar(GUI_NAME, buffer, 10000);
-	}
+		hueVal = hsvVal[0];
+	// Point2d point_meters = imageToWorldCoordinates(Point2d(x, y));
+	//sprintf(buffer, "Location: (%.3f m, %.3f m)", point_meters.x, point_meters.y);
 }
 
 int main(int argc, char **argv)
